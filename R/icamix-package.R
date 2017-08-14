10bc64f49f684144f940fa40749350d431f92110
xiaotianzhu <xiaotian.zhu.psualum@gmail.com>
2017-08-13 20:55:46 -0700

    Moving to roxygen for documentation.

---
+++ R/icamix-package.R
@@ -0,0 +1,47 @@
+
+
+#' Kernels for 'icamix' Functions
+#' 
+#' Internal kernel functions for the package \code{icamix}.
+#' 
+#' These are usually not to be called by the user.
+#' 
+#' @aliases EMInterwovenFastICA WtsFastICA WtsKde restore_rng save_rng
+#' @keywords internal
+NULL
+
+
+
+
+
+#' icamix: Estimation for ICA Mixture Model
+#' 
+#' Provides R functions which facilitate the estimation of ICA mixture models.
+#' This is a new tool for unsupervised clustering.
+#' 
+#' \tabular{ll}{ Package: \tab icamix\cr Type: \tab Package\cr Version: \tab
+#' 1.0.6\cr Date: \tab 2017-04-16\cr License: \tab GPL-2 | GPL-3\cr } We have
+#' developed the NSMM-ICA algorithm which is implemented in the main function
+#' EMFASTICAALG that currently integrates npEM and Fast-ICA for non-parametric
+#' estimation of ICA mixture models.
+#' 
+#' @name icamix-package
+#' @aliases icamix-package icamix
+#' @docType package
+#' @author Xiaotian Zhu, David R. Hunter
+#' 
+#' Maintainer: Xiaotian Zhu <xzhu@@natera.com>
+#' @references
+#' \href{http://www.tandfonline.com/doi/abs/10.1080/10485252.2016.1225049}{Zhu,
+#' Xiaotian, and David R. Hunter. "Theoretical grounding for estimation in
+#' conditional independence multivariate finite mixture models." Journal of
+#' Nonparametric Statistics 28.4 (2016): 683-701.}
+#' 
+#' \href{https://arxiv.org/abs/1510.08178}{Zhu, Xiaotian, and David R. Hunter.
+#' "Clustering Via Finite Nonparametric ICA Mixture Models." arXiv preprint
+#' arXiv:1510.08178}
+#' @keywords package
+NULL
+
+
+
